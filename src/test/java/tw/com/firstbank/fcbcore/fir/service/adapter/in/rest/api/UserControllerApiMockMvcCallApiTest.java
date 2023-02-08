e9c51307189493822542058e1c43707ed6b90aa2
Tabris <tabris.chang@hy-tech.com.tw>
2023-02-08 16:22:51 +0800

    [DEV][開發名稱: 範例程式] 1. UserApi mockmvc

---
+++ src/test/java/tw/com/firstbank/fcbcore/fir/service/adapter/in/rest/api/UserControllerApiMockMvcCallApiTest.java
@@ -0,0 +1,169 @@
+package tw.com.firstbank.fcbcore.fir.service.adapter.in.rest.api;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import tw.com.firstbank.fcbcore.fir.service.ServiceApplication;
+import tw.com.firstbank.fcbcore.fir.service.adapter.in.rest.mapper.UserControllerMapper;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.CreateUserResponseCommand;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.CreateUserUseCaseApi;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.DeleteUserResponseCommand;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.DeleteUserUseCaseApi;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.GetUserResponseCommand;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.GetUserUseCaseApi;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.GetUsersByBranchCodeUseCaseApi;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.GetUsersResponseCommand;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.GetUsersUseCaseApi;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.UpdateUserResponseCommand;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.UpdateUserUseCaseApi;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = ServiceApplication.class)
+public class UserControllerApiMockMvcCallApiTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+  @Autowired
+  private UserControllerMapper mapper;
+  @MockBean
+  private CreateUserUseCaseApi createUserApi;
+  @MockBean
+  private GetUserUseCaseApi getUserApi;
+  @MockBean
+  private GetUsersUseCaseApi getAllUserApi;
+  @MockBean
+  private DeleteUserUseCaseApi deleteUserApi;
+  @MockBean
+  private UpdateUserUseCaseApi updateUserApi;
+  @MockBean
+  private GetUsersByBranchCodeUseCaseApi getUsersByBranchCodeApi;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  private final static String BASE_URL = "/v1/users";
+
+  private final static String REST_URL = String.format("%1$s/%2$s/%3$s", BASE_URL, Instancio.create(String.class),
+      Instancio.create(String.class));
+
+  @Test
+  public void 當取得所有使用者時應要呼叫UseCase的execute() throws Exception {
+    //AAA
+    //Arrange
+    GetUsersResponseCommand responseCommand = Instancio.create(GetUsersResponseCommand.class);
+    when(getAllUserApi.execute(any())).thenReturn(responseCommand);
+    String expected = objectMapper.writeValueAsString(mapper.toGetUsersResponse(responseCommand));
+
+    //Act
+    ResultActions resultActions = mockMvc.perform(get(BASE_URL));
+
+    //Assert
+    assertResponse(expected, resultActions);
+
+  }
+
+  @Test
+  public void 當新增使用者時應要呼叫UseCase的execute() throws Exception {
+    //AAA
+    //Arrange
+    CreateUserRequest request = Instancio.create(CreateUserRequest.class);
+    String requestJson = objectMapper.writeValueAsString(request);
+    CreateUserResponseCommand response = Instancio.create(CreateUserResponseCommand.class);
+    response.setBranchCode(request.getBranchCode());
+    when(createUserApi.execute(any())).thenReturn(response);
+    String expected = objectMapper.writeValueAsString(mapper.toCreateUserResponse(response));
+
+    //Act
+    ResultActions resultActions = mockMvc.perform(
+        post(BASE_URL).contentType(APPLICATION_JSON_VALUE).content(requestJson));
+
+    //Assert
+    assertResponse(expected, resultActions);
+  }
+
+
+  @Test
+  public void 當取得使用者時應要呼叫UseCase的execute() throws Exception {
+    //AAA
+    //Arrange
+    GetUserResponseCommand response = Instancio.create(GetUserResponseCommand.class);
+    when(getUserApi.execute(any())).thenReturn(response);
+    String expected = objectMapper.writeValueAsString(mapper.toGetUserResponse(response));
+
+    //Act
+    ResultActions resultActions = mockMvc.perform(get(REST_URL));
+
+    //Assert
+    assertResponse(expected, resultActions);
+  }
+
+  @Test
+  public void 當刪除使用者時應要呼叫UseCase的execute() throws Exception {
+    //AAA
+    //Arrange
+    DeleteUserResponseCommand response = Instancio.create(DeleteUserResponseCommand.class);
+    when(deleteUserApi.execute(any())).thenReturn(response);
+    String expected = objectMapper.writeValueAsString(mapper.toDeleteUserResponse(response));
+
+    //Act
+    ResultActions resultActions = mockMvc.perform(delete(REST_URL));
+
+    //Assert
+    assertResponse(expected, resultActions);
+  }
+
+  @Test
+  public void 當修改使用者時應要呼叫UseCase的execute() throws Exception {
+    //AAA
+    //Arrange
+    UpdateUserRequest request = Instancio.create(UpdateUserRequest.class);
+    String requestJson = objectMapper.writeValueAsString(request);
+    UpdateUserResponseCommand response = Instancio.create(UpdateUserResponseCommand.class);
+    when(updateUserApi.execute(any())).thenReturn(response);
+    String expected = objectMapper.writeValueAsString(mapper.toUpdateUserResponse(response));
+
+    //Act
+    ResultActions resultActions = mockMvc.perform(
+        put(REST_URL).contentType(APPLICATION_JSON_VALUE).content(requestJson));
+
+    //Assert
+    assertResponse(expected, resultActions);
+  }
+
+  @Test
+  public void 當使用branchCode時取得使用者時應要呼叫UseCase的execute() throws Exception {
+    //AAA
+    //Arrange
+    GetUsersResponseCommand responseCommand = Instancio.create(GetUsersResponseCommand.class);
+    when(getUsersByBranchCodeApi.execute(any())).thenReturn(responseCommand);
+    String expected = objectMapper.writeValueAsString(mapper.toGetUsersResponse(responseCommand));
+    String url = BASE_URL + "/" + Instancio.create(String.class);
+
+    //Act
+    ResultActions resultActions = mockMvc.perform(get(url));
+
+    //Assert
+    assertResponse(expected, resultActions);
+  }
+
+  private static void assertResponse(String expected, ResultActions resultActions) throws Exception {
+    resultActions.andExpect(status().isOk()).andExpect(content().json(expected));
+  }
+
+}
---
+++ src/test/java/tw/com/firstbank/fcbcore/fir/service/adapter/in/rest/api/UserControllerApiMockMvcTest.java
@@ -0,0 +1,148 @@
+package tw.com.firstbank.fcbcore.fir.service.adapter.in.rest.api;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import tw.com.firstbank.fcbcore.fir.service.ServiceApplication;
+import tw.com.firstbank.fcbcore.fir.service.adapter.in.rest.mapper.UserControllerMapper;
+import tw.com.firstbank.fcbcore.fir.service.application.in.core.UseCaseApi;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.CreateUserResponseCommand;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.CreateUserUseCaseApi;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.DeleteUserResponseCommand;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.DeleteUserUseCaseApi;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.GetUserResponseCommand;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.GetUserUseCaseApi;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.GetUsersByBranchCodeUseCaseApi;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.GetUsersResponseCommand;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.GetUsersUseCaseApi;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.UpdateUserResponseCommand;
+import tw.com.firstbank.fcbcore.fir.service.application.in.user.api.UpdateUserUseCaseApi;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = ServiceApplication.class)
+public class UserControllerApiMockMvcTest {
+
+  @Autowired
+  private UserControllerApi userApi;
+  @Autowired
+  private UserControllerMapper mapper;
+  @MockBean
+  private CreateUserUseCaseApi createUserApi;
+  @MockBean
+  private GetUserUseCaseApi getUserApi;
+  @MockBean
+  private GetUsersUseCaseApi getAllUserApi;
+  @MockBean
+  private DeleteUserUseCaseApi deleteUserApi;
+  @MockBean
+  private UpdateUserUseCaseApi updateUserApi;
+  @MockBean
+  private GetUsersByBranchCodeUseCaseApi getUsersByBranchCodeApi;
+
+
+  @Test
+  public void 當取得所有使用者時應要呼叫UseCase的execute() {
+    //AAA
+    //Arrange
+    GetUsersResponseCommand responseCommand = Instancio.create(GetUsersResponseCommand.class);
+    GetUsersResponse expected = mapper.toGetUsersResponse(responseCommand);
+    when(getAllUserApi.execute(any())).thenReturn(responseCommand);
+
+    //Act
+    GetUsersResponse actual = userApi.getUsers();
+
+    //Assert
+    assertResponse(getAllUserApi, actual, expected);
+  }
+
+  @Test
+  public void 當新增使用者時應要呼叫UseCase的execute() {
+    //AAA
+    //Arrange
+    CreateUserResponseCommand response = Instancio.create(CreateUserResponseCommand.class);
+    CreateUserResponse expected = mapper.toCreateUserResponse(response);
+    when(createUserApi.execute(any())).thenReturn(response);
+
+    //Act
+    CreateUserResponse actual = userApi.createUser(any());
+
+    //Assert
+    assertResponse(createUserApi, actual, expected);
+  }
+
+  @Test
+  public void 當取得使用者時應要呼叫UseCase的execute() {
+    //AAA
+    //Arrange
+    GetUserResponseCommand response = Instancio.create(GetUserResponseCommand.class);
+    GetUserResponse expected = mapper.toGetUserResponse(response);
+    when(getUserApi.execute(any())).thenReturn(response);
+
+    //Act
+    GetUserResponse actual = userApi.getUser(response.getBranchCode(), response.getNo());
+
+    //Assert
+    assertResponse(getUserApi, actual, expected);
+  }
+
+  @Test
+  public void 當刪除使用者時應要呼叫UseCase的execute() {
+    //AAA
+    //Arrange
+    DeleteUserResponseCommand response = Instancio.create(DeleteUserResponseCommand.class);
+    DeleteUserResponse expected = mapper.toDeleteUserResponse(response);
+    when(deleteUserApi.execute(any())).thenReturn(response);
+
+    //Act
+    DeleteUserResponse actual = userApi.deleteUser(Instancio.create(String.class), Instancio.create(String.class));
+
+    //Assert
+    assertResponse(deleteUserApi, actual, expected);
+  }
+
+  @Test
+  public void 當修改使用者時應要呼叫UseCase的execute() {
+    //AAA
+    //Arrange
+    UpdateUserResponseCommand response = Instancio.create(UpdateUserResponseCommand.class);
+    UpdateUserResponse expected = mapper.toUpdateUserResponse(response);
+    when(updateUserApi.execute(any())).thenReturn(response);
+
+    //Act
+    UpdateUserResponse actual = userApi.updateUser(Instancio.create(String.class), Instancio.create(String.class), any());
+
+    //Assert
+    assertResponse(updateUserApi, actual, expected);
+  }
+
+  @Test
+  public void 當使用branchCode時取得使用者時應要呼叫UseCase的execute() {
+    //AAA
+    //Arrange
+    GetUsersResponseCommand responseCommand = Instancio.create(GetUsersResponseCommand.class);
+    GetUsersResponse expected = mapper.toGetUsersResponse(responseCommand);
+    when(getUsersByBranchCodeApi.execute(any())).thenReturn(responseCommand);
+
+    //Act
+    GetUsersResponse actual = userApi.getUsersByBranchCode(Instancio.create(String.class));
+
+    //Assert
+    assertResponse(getUsersByBranchCodeApi, actual, expected);
+  }
+
+  public <T> void assertResponse(UseCaseApi useCaseApi, T actual, T expected) {
+    verify(useCaseApi).execute(any());
+    assertThat(actual)
+        .usingRecursiveComparison()
+        .isEqualTo(expected);
+  }
+
+}
